03cebd25181b29ed530c8f20d4339fff75178fa8
GeniusFunny <geniusfunnyyh@gmail.com>
2018-11-25 23:57:18 +0800

    add h()/createElement()

--- src/vnode.ts
+++ src/vnode.ts
@@ -1,3 +1,11 @@
-export interface VNode {
-  (): void
+export interface Attributes {
+  children?: string | VNode,
+  key?: string | number
+}
+
+export class VNode{
+  nodeName: string
+  children?: any
+  attributes?: Attributes
+  key?: string | number
 }
